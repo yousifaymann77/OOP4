b8c71f2102e882cbd1c8434386464ea554a0ff66
yousif <yshehab693@gmail.com>
2026-09-19 00:31:18 +0300

    Part01 Q1&2 Theoritical Part

--- oop4Assign/Program.cs
+++ oop4Assign/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
+            #region Part01 Theoritical 
 
+            #region Q1 a)What is Abstraction in Object-Oriented Programming?
+            //Abstraction is the process of hiding the implementation details and showing only the essential features to the user 
+            #endregion
+
+            #region Q1 b) Why is abstraction considered one of the four pillars of OOP?
+            // Because it helps us to focus only on what an object does rather than how it does it which reduces complexity and make code easier to use 
+            #endregion
+
+            #region Q2 a) What is the difference between an Abstract Class and an Interface?
+            // Abstract class can have fields andconstructors But the Interface does not and Abstract class is a base class type , interface represents only a contract
+            #endregion
+
+            #region Q2 b)  When would you choose an Interface instead of an Abstract Class?
+            // when I want to define a contract or capability rather than a shared base class with common implementation
+            #endregion
+
+            #region Q2 c) Can a class inherit from multiple abstract classes? Can it implement multiple interfaces?
+             // Class can Inherit from one abstract class But can implement multiple Interfaces
+            #endregion
+
+            #endregion
         }
     }
 }
