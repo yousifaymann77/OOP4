5081b67dabcec9ef0f2b86464622cce44de0074d
yousif <yshehab693@gmail.com>
2026-09-19 01:15:29 +0300

    Part02 Q6 Delivery Report Class

---
+++ oop4Assign/DeliveryReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop4Assign
+{
+    #region part02 Q6
+    internal class DeliveryReport
+    {
+        public void Printshipment(ITrackable shipment)
+        {
+            Console.WriteLine($"Tracking Status : {shipment.GetTrackingStatus}");
+        }
+        public void PrintInsurance(IInsurable shipment)
+        {
+            Console.WriteLine($"Shipment Insurance : {shipment.CalculateInsurance}");
+        }
+    } 
+    #endregion
+}
